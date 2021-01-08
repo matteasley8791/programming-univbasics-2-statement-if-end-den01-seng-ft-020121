ccd0457607f656e312de9063ef7e5267d458f347
Matthew Easley <matteasley8791@gmail.com>
2021-01-08 19:10:05 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,8 @@
-current_time = time.now 
-current_time = current_time.to_i 
-
+current_time = time.now
+current_time = current_time.to_i
+ 
 if current_time.even?
-  puts "even"
+  puts "Even!"
 else
-  puts "odd"
+  puts "Odd!"
 end
